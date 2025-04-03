743ceb25cdfe21585dfb4a24fbdaf47b6a19a29d
seanfourman <Sean.Fourman@Ruppin365.net>
2025-04-04 00:49:21 +0300

    Added search functionality for My Movies tab

--- Frontend/scripts/myMovies.js
+++ Frontend/scripts/myMovies.js
@@ -1,6 +1,20 @@
 $(document).ready(init);
 
 function init() {
+  $("#searchInputs input").on("keypress", function (e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      $(".movieCard").remove();
+      triggerSearch();
+    }
+  });
+
+  $("#searchInputs input").on("blur", function () {
+    $(".movieCard").remove();
+    triggerSearch();
+  });
+
+  setupSearchTitlePlaceholder();
   getUserMovies();
 }
 
@@ -9,13 +23,45 @@ function getUserMovies() {
 }
 
 function readSCB(res) {
-  //console.log(res);
-  loadMovies(res); // Wait until the data is ready
-  if (res.length != 0) {
-    console.log("not null");
-  }
+  loadMovies(res);
 }
 
 function readECB(err) {
   console.log(err);
+}
+
+function triggerSearch() {
+  const title = $("#searchTitle").val().trim();
+  const startDate = $("#startDate").val();
+  const endDate = $("#endDate").val();
+
+  $(".movieCard").remove();
+  if (title) {
+    ajaxCall("GET", `${url}/searchByTitle`, { title }, successCB, errorCB);
+  } else if (startDate && endDate) {
+    ajaxCall("GET", `${url}/searchByReleaseDate/startDate/${startDate}/endDate/${endDate}`, null, successCB, errorCB);
+  } else {
+    getUserMovies();
+  }
+}
+
+function successCB(res) {
+  $(".movieCard").remove();
+  loadMovies(res);
+}
+
+function errorCB(err) {
+  console.log(err);
+}
+
+function setupSearchTitlePlaceholder() {
+  $("#searchTitle").on("focus", function () {
+    $(this).attr("placeholder", "");
+  });
+
+  $("#searchTitle").on("blur", function () {
+    if ($(this).val().trim() === "") {
+      $(this).attr("placeholder", "Search");
+    }
+  });
 }
