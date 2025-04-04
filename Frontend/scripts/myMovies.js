67eedd81006dfaf688cff20001486ea87b16377b
seanfourman <Sean.Fourman@Ruppin365.net>
2025-04-04 11:05:52 +0300

    Added server error messages

--- Frontend/scripts/myMovies.js
+++ Frontend/scripts/myMovies.js
@@ -1,5 +1,23 @@
 $(document).ready(init);
 
+const messages = [
+  "The movies ghosted us.",
+  "Popcorn machine broke down.",
+  "We forgot the script.",
+  "Server went on vacation.",
+  "Our reel got jammed.",
+  "Lost in the credits.",
+  "No signal from Hollywood.",
+  "Buffering... forever apparently.",
+  "Plot twist: no connection.",
+  "The page fell asleep.",
+  "Movie magic malfunctioned. Oops!",
+  "Be kind, rewind... retry!",
+  "The drama's behind the scenes.",
+  "We paused the internet.",
+  "Couch potato panic mode!"
+];
+
 function init() {
   $("#searchInputs input").on("keypress", function (e) {
     if (e.key === "Enter") {
@@ -26,8 +44,9 @@ function readSCB(res) {
   loadMovies(res);
 }
 
-function readECB(err) {
-  console.log(err);
+function readECB() {
+  showPopup("Failed to reach server. Please try again later!", false);
+  showNoMoviesMessage(messages[Math.floor(Math.random() * messages.length)]);
 }
 
 function triggerSearch() {
@@ -51,8 +70,9 @@ function successCB(res) {
   loadMovies(res);
 }
 
-function errorCB(err) {
-  console.log(err);
+function errorCB() {
+  console.log("alo");
+  showPopup("Failed to reach server. Please try again later!", false);
 }
 
 function setupSearchTitlePlaceholder() {
