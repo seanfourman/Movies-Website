30c0d2de6d0336b05003071bf076888e78d28b4e
seanfourman <seanfourman@ruppin365.net>
2025-05-12 11:08:32 +0300

    sql update

--- SQL/GetUniqueLanguagesAndGenres.sql
+++ SQL/GetUniqueLanguagesAndGenres.sql
@@ -7,7 +7,38 @@ BEGIN
     FROM MoviesTable
     WHERE language IS NOT NULL AND language <> '';
 
-    SELECT DISTINCT LTRIM(RTRIM(genres)) AS Genre
-    FROM MoviesTable
-    WHERE genres IS NOT NULL AND genres <> '';
+    DECLARE @GenreString NVARCHAR(MAX);
+    DECLARE @Genre NVARCHAR(100);
+
+    CREATE TABLE #Genres (Genre NVARCHAR(100));
+
+    DECLARE genre_cursor CURSOR FOR
+        SELECT genres FROM MoviesTable WHERE genres IS NOT NULL;
+
+    OPEN genre_cursor;
+    FETCH NEXT FROM genre_cursor INTO @GenreString;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        WHILE CHARINDEX(',', @GenreString) > 0
+        BEGIN
+            SET @Genre = LTRIM(RTRIM(LEFT(@GenreString, CHARINDEX(',', @GenreString) - 1)));
+            IF @Genre <> ''
+                INSERT INTO #Genres (Genre) VALUES (@Genre);
+            SET @GenreString = SUBSTRING(@GenreString, CHARINDEX(',', @GenreString) + 1, LEN(@GenreString));
+        END
+
+        SET @Genre = LTRIM(RTRIM(@GenreString));
+        IF @Genre <> ''
+            INSERT INTO #Genres (Genre) VALUES (@Genre);
+
+        FETCH NEXT FROM genre_cursor INTO @GenreString;
+    END
+
+    CLOSE genre_cursor;
+    DEALLOCATE genre_cursor;
+
+    SELECT DISTINCT Genre FROM #Genres ORDER BY Genre;
+
+    DROP TABLE #Genres;
 END
